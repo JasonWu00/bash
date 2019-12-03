41f26793383234e7aba22ca72c2043dd62e8ff4d
JasonWu00 <zwu00@stuy.edu>
2019-12-03 10:54:44 -0500

    some more things

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int main() {
-
+  char input[100];
+  fgets(input, 100, stdin);
+  char *position;
+  if ((position=strchr(input, '\n')) != NULL) {
+    *position = '\0';
+  }
 }

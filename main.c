7e87978739e0abdadf8b4591ee268feb23298d80
JasonWu00 <zwu00@stuy.edu>
2019-12-03 17:13:28 -0500

    Added files from parse homework as helpers for the shell

--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "parse.h"
 
-int main() {
-  char input[100];
-  fgets(input, 100, stdin);
-  char *position;
-  if ((position=strchr(input, '\n')) != NULL) {
-    *position = '\0';
+void main() {
+  char input[100] = "";
+
+  while (strcmp(input, "exit") != 0) {
+    fgets(input, 100, stdin);
+    char *position;
+    if ((position = strchr(input, "\n")) != NULL) {
+      *position = "\0";
+    }
   }
+  exit(0); //input is equal to "exit", closing the shell
 }

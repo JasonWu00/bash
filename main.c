e8aea1f9b23caf0a0c8dba4bc83aabf6cd3d0dcf
JasonWu00 <zwu00@stuy.edu>
2019-12-05 11:00:34 -0500

    start of shell name before every user entry

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/wait.h>
 #include "parse.h"
 
 int main() {
@@ -13,9 +14,9 @@ int main() {
   char input[100];
   char ** output;
   int parentPID = getpid();
+  printf("WISH > ");
 
   while (1) {//as long as "exit" isn't entered
-    printf("\n");
     fgets(input, 100, stdin);
     char *position;
     if ((position = strchr(input, '\n')) != NULL) {
@@ -27,24 +28,33 @@ int main() {
     output = parse_args(pointer);
     char cd[3] = "cd";
     char leave[5] = "exit";
+    const char *cd_p = &cd;
+    const char *leave_p = &leave;
 
-    if (strstr(input, &cd) != NULL) {//inputted command has a "cd" in it
+    if (strstr(input, cd_p) != NULL) {//inputted command has a "cd" in it
       chdir(output[1]);
+      printf("\nWISH > ");
     }
-    else if (strstr(input, &leave) != NULL) {
-      printf("Exiting shell\n");
+    else if (strstr(input, leave_p) != NULL) {
+      printf("WISH > Exiting shell\n");
       exit(0);
     }
     else {
       fork();//child process will execvp and end, parent keeps running
+      if (getpid() == parentPID) {
+        printf("\n");
+      }
+      wait(NULL);
       if (getppid() == parentPID) {
         int execute_return;
+        printf("WISH > ");
         execute_return = execvp(output[0], output);
-        if(execute_return < 0) {
-          printf("Error encountered: %i (%s)\n", errno, strerror(errno));
+        if (execute_return < 0) {
+          printf("WISH > Error encountered: %i (%s)\n", errno, strerror(errno));
         }
         exit(0);
       }
+      printf("\nWISH > ");
     }
   }
 }

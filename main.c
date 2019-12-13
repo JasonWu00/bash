5dd05940a83a7884edc1638666ff96593f3889af
Julian Wong <jwong01@stuy.edu>
2019-12-13 10:46:48 -0500

    merge some random changes?

--- main.c
+++ main.c
@@ -66,8 +66,8 @@ int main() {
       outstruct = parse_args(pointer);
       outstruct.lastToken--;
 
-      printf("DEBUG: your cmd: %s, last_token: %i\n", commandArray[q], outstruct.lastToken);
-      printf("Command has > sign: %i, 0 if DNE\n", strstr(outstruct.output[outstruct.lastToken-1], more_p) != NULL);
+      //printf("DEBUG: your cmd: %s, last_token: %i\n", commandArray[q], outstruct.lastToken);
+      //printf("DEBUG: Command has > sign: %i, 0 if DNE\n", strstr(outstruct.output[outstruct.lastToken-1], more_p) != NULL);
       if (strstr(commandArray[q], cd_p) != NULL) {//input command has a "cd" in it
         chdir(outstruct.output[1]);
         printf("\nWISH > ");
@@ -76,23 +76,24 @@ int main() {
         printf("\nWISH > Exiting shell\nThank you for visiting! Come again soon!\n\n");
         exit(0);
       }
-      else if (strstr(outstruct.output[outstruct.lastToken-1], more_p) != NULL) {
-        printf("DEBUG: value of last token: %s\n", outstruct.output[outstruct.lastToken]);
+      else if (strstr(outstruct.output[outstruct.lastToken-1], more_p) != NULL) {//if second last token is >
+        //printf("DEBUG: value of last token: %s\n", outstruct.output[outstruct.lastToken]);
         int fd_redirectedFile = open(outstruct.output[outstruct.lastToken], O_RDWR);//to see if file exists
         if (fd_redirectedFile == -1) {//if no such file
           //create a file
-          printf("DEBUG: file DNE\n");
-          printf("DEBUG: fd_redir has this value: %i\n", fd_redirectedFile);
+          //printf("DEBUG: file DNE\n");
+          //printf("DEBUG: fd_redir has this value: %i\n", fd_redirectedFile);
           fd_redirectedFile = open(outstruct.output[outstruct.lastToken], O_RDWR | O_CREAT);
           //int filedesc = open(outstruct.output[outstruct.lastToken],O_RDWR | O_CREAT);
-          exit(0);
+          //exit(0);
         }
         else {//file exists
-          printf("DEBUG: file exists\n");
-          printf("DEBUG: fd_redir has this value: %i\n", fd_redirectedFile);
+          //printf("DEBUG: file exists\n");
+          //printf("DEBUG: fd_redir has this value: %i\n", fd_redirectedFile);
           //int filedesc = open(outstruct.output[outstruct.lastToken],O_RDWR);
-          exit(0);
+          //exit(0);
         }
+        dup2(1,fd_redirectedFile);
       }
       else {
         fork();//child process will execvp and end, parent keeps running
